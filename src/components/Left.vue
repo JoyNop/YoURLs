784f3b388b1fcb59f7ed228874d00daa88bed070
HanRui <MAIL@HR.IM>
2020-09-21 15:07:29 +0800

    1

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
- 
-  </div>
+  <div>Left</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Left',
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ 
+<Left/>
+    <Right/>
+ 
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Left from '@/components/Left.vue'
+import Right from '@/components/Right.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Left,Right
   }
 }
 </script>
+<style lang="scss" scoped>
+.home{
+  display:flex;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
